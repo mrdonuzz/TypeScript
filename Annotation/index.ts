d0940a43802efc2a77e265152ae13297b1fa21e3
mrdonuzz <50772489+mrdonuzz@users.noreply.github.com>
2025-04-17 23:55:33 +0700

    Initial commit

---
+++ Annotation/index.ts
@@ -0,0 +1,31 @@
+//1
+let username: string = "Donuzz";
+let score: number = 89.5;
+let isLoggedIn: boolean = true;
+
+//2
+function greet(name: string): string {
+  return `Hello, ${name}!`;
+}
+
+console.log(greet("Donuzz"));
+
+//3
+function square(n: number): number {
+  return n * n;
+}
+
+console.log(5);
+
+//4
+let age: number = 25;
+age = 30;
+
+function showStatus(status: boolean): boolean {
+  return status;
+}
+
+//5
+const message = function (message: string): string {
+  return message;
+};
